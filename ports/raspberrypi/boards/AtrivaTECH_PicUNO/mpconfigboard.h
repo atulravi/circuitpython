0d0dadbaed995edfb35e03a7d0e5d3c264047329
atulravi <atul@esccrasci.in>
2024-04-04 21:21:37 +0530

    New CircuitPython compatible Board

---
+++ ports/raspberrypi/boards/AtrivaTECH_PicUNO/mpconfigboard.h
@@ -0,0 +1,11 @@
+#define MICROPY_HW_BOARD_NAME "AtrivaTECH-PicUNO"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define MICROPY_HW_LED_STATUS (&pin_GPIO14)
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO17)
+
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO21)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO20)
+
+#define DEFAULT_UART_BUS_RX (&pin_GPIO1)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO0)
